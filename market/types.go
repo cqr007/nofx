a7b237848852b1de8449b41969587ee3707fbbdc
Jonny Cifuentes <61655749+cqr007@users.noreply.github.com>
2025-12-17 12:23:22 +0800

    Update types.go

--- market/types.go
+++ market/types.go
@@ -12,6 +12,9 @@ type Data struct {
 	CurrentEMA20      float64
 	CurrentMACD       float64
 	CurrentRSI7       float64
+	MA5               float64
+    MA34              float64
+    MA170             float64
 	OpenInterest      *OIData
 	FundingRate       float64
 	IntradaySeries    *IntradayData
