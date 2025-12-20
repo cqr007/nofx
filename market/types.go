3354abd1c0cac40af003d54784235941c126bb42
flamekeep <chenqiruir600@qq.com>
2025-12-20 11:49:37 +0800

    Rename MidTermSeries and update price change fields

--- market/types.go
+++ market/types.go
@@ -19,7 +19,7 @@ type Data struct {
 	OpenInterest      *OIData
 	FundingRate       float64
 	IntradaySeries    *IntradayData
-	MidTermSeries15m  *MidTermData15m
+	MidTermSeries30m  *MidTermData30m
 	MidTermSeries1h   *MidTermData1h
 	LongerTermContext *LongerTermData
 	DailyContext      *DailyData
@@ -74,8 +74,8 @@ type IntradayData struct {
 	SeriesFields // 嵌入共享字段
 }
 
-// MidTermData15m 中期数据(15分钟间隔)
-type MidTermData15m struct {
+// MidTermData30m 中期数据(30分钟间隔)
+type MidTermData30m struct {
 	SeriesFields // 嵌入共享字段
 }
 
@@ -148,7 +148,7 @@ type SymbolFeatures struct {
 	Symbol           string    `json:"symbol"`
 	Timestamp        time.Time `json:"timestamp"`
 	Price            float64   `json:"price"`
-	PriceChange15Min float64   `json:"price_change_15min"`
+	PriceChange30Min float64   `json:"price_change_30min"`
 	PriceChange1H    float64   `json:"price_change_1h"`
 	PriceChange4H    float64   `json:"price_change_4h"`
 	Volume           float64   `json:"volume"`
@@ -182,7 +182,7 @@ type Config struct {
 
 type AlertThresholds struct {
 	VolumeSpike      float64 `json:"volume_spike"`
-	PriceChange15Min float64 `json:"price_change_15min"`
+	PriceChange30Min float64 `json:"price_change_30min"`
 	VolumeTrend      float64 `json:"volume_trend"`
 	RSIOverbought    float64 `json:"rsi_overbought"`
 	RSIOversold      float64 `json:"rsi_oversold"`
@@ -197,7 +197,7 @@ type CleanupConfig struct {
 var config = Config{
 	AlertThresholds: AlertThresholds{
 		VolumeSpike:      3.0,
-		PriceChange15Min: 0.05,
+		PriceChange30Min: 0.05,
 		VolumeTrend:      2.0,
 		RSIOverbought:    70,
 		RSIOversold:      30,
